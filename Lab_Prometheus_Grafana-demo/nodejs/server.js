9fc7ec65c8a303c441d532e8a416bc9a8a56f1e1
hoangpt2991 <59075275+hoangpt2991@users.noreply.github.com>
2023-06-07 17:25:12 +0700

    Add files via upload

---
+++ Lab_Prometheus_Grafana-demo/nodejs/server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+// Define a simple endpoint to retrieve a list of items
+app.get('/items', (req, res) => {
+  const items = [
+    { id: 1, name: 'Item 1', description: 'Description for Item 1' },
+    { id: 2, name: 'Item 2', description: 'Description for Item 2' },
+    { id: 3, name: 'Item 3', description: 'Description for Item 3' },
+  ];
+  res.json(items);
+});
+
+
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+
